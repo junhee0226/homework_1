3cf99f1376d67eae49c5166b21f273ccd236c6da
junhee0226 <167046265+junhee0226@users.noreply.github.com>
2024-05-14 10:22:55 +0900

    JHL_Total
    애니메이션, 캐릭터 이동, 타일맵 등등

---
+++ Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    private Camera camera;
+    public Transform target;
+
+    public float smoothSpeed = 3;
+    public Vector2 offset;
+    public float limitMinX, limitMaxX, limitMinY, limitMaxY;
+    float cameraHalfWidth, cameraHalfHeight;
+
+    private void Awake()
+    {
+        camera = Camera.main;
+    }
+
+    private void Start()
+    {
+        cameraHalfWidth = camera.aspect * camera.orthographicSize;
+        cameraHalfHeight = camera.orthographicSize;
+    }
+
+    private void LateUpdate()
+    {
+        Vector3 desiredPosition = new Vector3(
+            Mathf.Clamp(target.position.x + offset.x, limitMinX + cameraHalfWidth, limitMaxX - cameraHalfWidth),   // X
+            Mathf.Clamp(target.position.y + offset.y, limitMinY + cameraHalfHeight, limitMaxY - cameraHalfHeight), // Y
+            -10);                                                                                                  // Z
+        transform.position = Vector3.Lerp(transform.position, desiredPosition, Time.deltaTime * smoothSpeed);
+    }
+}
---
+++ Assets/Scripts/PlayerLook.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+
+public class PlayerLook : MonoBehaviour
+{
+    Transform tf;
+    private Camera camera;
+
+    private void Awake()
+    {
+        camera = Camera.main;
+    }
+    void Start()
+    {
+        tf = transform;
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        Vector2 mousePos = Input.mousePosition;
+        Vector3 Player = Camera.main.ScreenToWorldPoint(mousePos);
+
+        if (Player.x < tf.position.x)
+        {
+            tf.localScale = new Vector3(-3, 3, 3);
+        }
+        else
+        {
+            tf.localScale = new Vector3(3, 3, 3);
+        }
+    }
+}
